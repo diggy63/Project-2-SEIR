b03a36a4a20179773cc540d1ffc84f235e570b8a
diggy63 <daniel.kanczugowski@gmail.com>
2022-04-14 14:02:17 -0700

    added final css and ready to present

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // replace your database connection string here
 //that the code for release database
-mongoose.connect(process.env.DATABASE_URL,{ 
-// mongoose.connect('mongodb://localhost/drinks9', {
+// mongoose.connect(process.env.DATABASE_URL,{ 
+mongoose.connect('mongodb://localhost/drinks9', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,52 @@
-const addI = document.querySelector('#addIngred');
-const ulElement = document.querySelector('#ingredientList');
-const newIngredAdd = document.querySelector('#newLiq');
-const newAmountAdd = document.querySelector('#newAmount');
-const ingredDisplay = document.querySelector('#drink-display');
-const getAll = document.querySelectorAll('.row');
-const wholeDoc = document.querySelector('.contain');
-
+const addI = document.querySelector("#addIngred");
+const ulElement = document.querySelector("#ingredientList");
+const newIngredAdd = document.querySelector("#newLiq");
+const newAmountAdd = document.querySelector("#newAmount");
+const ingredDisplay = document.querySelector("#drink-display");
+const getAll = document.querySelectorAll(".row");
+const wholeDoc = document.querySelector(".contain");
 
 // if(getAll.length > 0 ){
 //     document.body.style.backgroundImage = "url('')";
 // }
-if(getAll.length > 2){
-    let count = getAll.length - 2
-    let startHeight = 1300;
-    for(let i = count; i>0; i-- ){
-        startHeight = startHeight + 300;
-        console.log("grow");
-        wholeDoc.style.height = `${startHeight}px`;
-    }
+if (getAll.length > 1) {
+     let count = getAll.length - 2;
+     let startHeight = 1000;
+     for (let i = count; i > 0; i--) {
+          startHeight = startHeight + 300;
+          console.log("grow");
+          wholeDoc.style.height = `${startHeight}px`;
+     }
 }
 
 console.log(wholeDoc.clientHeight);
 
-if(addI){
-addI.addEventListener("click", addIngredient);
+if (addI) {
+     addI.addEventListener("click", addIngredient);
 }
 
-function addIngredient(e){
-    const valEl = newIngredAdd.value;
-    const valAmount = newAmountAdd.value
+function addIngredient(e) {
+     const valEl = newIngredAdd.value;
+     const valAmount = newAmountAdd.value;
 
+     const newEl = document.createElement("INPUT");
+     newEl.setAttribute("type", "text");
+     newEl.setAttribute("name", "ingredients");
+     newEl.setAttribute("value", valEl);
+     ulElement.appendChild(newEl);
 
+     const newEl2 = document.createElement("INPUT");
+     newEl2.setAttribute("type", "text");
+     newEl2.setAttribute("name", "amount");
+     newEl2.setAttribute("value", valAmount);
+     ulElement.appendChild(newEl2);
 
-    const newEl = document.createElement("INPUT");
-    newEl.setAttribute("type","text");
-    newEl.setAttribute("name","ingredients");
-    newEl.setAttribute("value", valEl);
-    ulElement.appendChild(newEl);
-
-
-    const newEl2 = document.createElement("INPUT");
-    newEl2.setAttribute("type","text");
-    newEl2.setAttribute("name","amount");
-    newEl2.setAttribute("value", valAmount);
-    ulElement.appendChild(newEl2);
-
-
-    
-    const newDisEl = document.createElement("li");
-    const text = document.createTextNode(`Ingredient: ${valEl}   Amount: ${valAmount}`);
-    newDisEl.appendChild(text);
-    ingredDisplay.appendChild(newDisEl);
+     const newDisEl = document.createElement("li");
+     const text = document.createTextNode(
+          `Ingredient: ${valEl} -  Amount: ${valAmount}`
+     );
+     const h3El = document.createElement("h3");
+     h3El.appendChild(text);
+     newDisEl.appendChild(h3El);
+     ingredDisplay.appendChild(newDisEl);
 }
